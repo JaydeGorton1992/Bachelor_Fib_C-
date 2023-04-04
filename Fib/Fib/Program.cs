1f0e2dfd34382c453a365ac3fceae26c3c623594
JaydeGorton1992 <116609587+JaydeGorton1992@users.noreply.github.com>
2023-04-05 08:54:53 +1000

    Add files via upload

---
+++ Fib/Fib/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fib
+{
+    //https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+    /*
+     * Jade Gorton u3158046
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int output = -1;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Write LineInput: ");
+                Int32.TryParse(Console.ReadLine(), out output);
+            } while (output <= 0);
+            if (output >= 30)
+                Console.WriteLine("Working, might Take long time: " + (Math.Pow(output,2)) + " processing steps");
+            Console.WriteLine(Fib(output));
+            Console.ReadLine();
+        }
+
+        public static int Fib(int n)
+        {
+            if (n < 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return (Fib(n-1) + Fib(n-2));
+        }
+    }
+}
